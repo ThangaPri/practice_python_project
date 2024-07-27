87a4b174b0fa964a951563acd6d5bbc2ed9cb308
ThangaPri <priya.thangam2023@gmail.com>
2024-07-26 23:46:01 -0400

    static code analysis

--- server.py
+++ server.py
@@ -2,12 +2,9 @@
     analysis to be executed over the Flask channel and deployed on
     localhost:5000.
 '''
-# Import Flask, render_template, request from the flask pramework package : TODO
-# Import the sentiment_analyzer function from the package created: TODO
 from flask import Flask,render_template,request
-
 from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
-#Initiate the flask app : TODO
+
 app=Flask(__name__,template_folder="templates")
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
@@ -17,25 +14,17 @@ def sent_analyzer():
         score for the provided text.
     '''
     text_to_analyze=request.args.get('textToAnalyze')
-
     response=sentiment_analyzer(text_to_analyze)
-   
     label=response['label']
     score=response['score']
+    lbldisplay=label.split('_')[1]
     if label is None:
         return "Invalid input!Try Again"
-
-    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
-
-
+    return f"The given text has been identified as {lbldisplay} with a score of {score}."
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
+        page over the Flask channel'''
     return render_template("index.html")
-
-if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
+if __name__=="__main__":
     app.run(host="0.0.0.0",port=5005,debug=True)
